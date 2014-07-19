d9d3ac2ef98832f85d932866c0da570c2b169c32
Christophe Tiraoui <krux@krux.fr>
2014-07-19 21:55:05 +0200

    Correction de la mécanique de slide et d'évaluation du classfile

--- OpenNetworkContainer/scripts/vendor/onc/ONC_Router.js
+++ OpenNetworkContainer/scripts/vendor/onc/ONC_Router.js
@@ -79,14 +79,14 @@ var ONC_Router = function (app) {
         }
 
 
-       
+
         //Avec parametres ?
         if (window.url("?", hash) == "") {
             param = null;
-           
+
         }
         else {
-            
+
             var url = window.url("?", hash);
             var pairs = url.split("&");
             var data = {};
@@ -136,8 +136,6 @@ var ONC_Router = function (app) {
                 throw "La page " + hash + " est introuvable ou n'est pas correctement incorporée";
             }
 
-
-
             //Dechargement de la page courante
             self.unloadCurrent(function () {
 
@@ -152,22 +150,28 @@ var ONC_Router = function (app) {
                         url: classfilePage,
                         cache: false
                     }).done(function (result) {
-                        //Chargement de la  définition de classe en mémoire
-                        eval(result);
+
+                        ONC_Logger.log("ROUTER: Classfile récuperé (" + pageId + ")");
 
 
-                    }).fail(function (xhr, e) {
-                        self.app.onerror(e);
-                    }).always(function () {
+                        ONC_Logger.log("ROUTER: Démarrage du sliding (" + pageId + ")");
 
                         //On slide vers cette nouvelle page
                         self.slider.slidePage($target, function () {
 
+                            ONC_Logger.log("ROUTER: Page slidée");
+
+                            //Chargement de la  définition de classe en mémoire
+                            eval(result);
+
                             //On tente d'initialiser la nouvelle page
-                            if (eval("typeof " + pageId + "_PageClass === 'undefined'") == false) {
+                            if (eval("typeof " + pageId + "_PageClass == 'undefined'") == false) {
                                 var pageInstance = eval("new " + pageId + "_PageClass()");
 
                                 self.currentPage = pageInstance;
+
+                                ONC_Logger.log("ROUTER: Page Instanciée (" + pageId + ")");
+
 
                                 //Binding de la page
                                 if (pageInstance != null && pageInstance.bind != null) {
@@ -179,13 +183,22 @@ var ONC_Router = function (app) {
                                     pageInstance.load(param);
                                 }
                             }
+                            else {
+                                ONC_Logger.warn("ROUTER: Définition du classfile introuvable (" + pageId + ")");
+                            }
 
 
                         });
 
+                    }).fail(function (xhr, e) {
+                        self.app.onerror(e);
+
+                        //On slide malgré tout
+                        self.slider.slidePage($target);
                     });
                 }
                 else {
+                    ONC_Logger.log("ROUTER: Slide sans classfile");
                     //On slide vers cette nouvelle page
                     self.slider.slidePage($target);
                     //Masque le spinner
